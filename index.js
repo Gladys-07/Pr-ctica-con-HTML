091878b4b0e5343f9156880adc7289b28b0fe73e
Gladys-07 <A00839182@tec.mx>
2025-06-06 17:37:12 -0600

    cambios para API

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("¡Hola Mundo!");
+import express from "express" ;
+
+const app = express() //instancia del servidor
+
+app.use("/", express.static("public")) 
+const port = 3000;
+app.listen(3000, console.log("http://localhost:" + port)) 
